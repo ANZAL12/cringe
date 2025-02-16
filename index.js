1d0c07ee2ac225e7ba4c1d1cef9b5b359677d144
ANZAL12 <mohammedanzel123@gmail.com.com>
2025-02-16 11:22:56 +0530

    final push

--- index.js
+++ index.js
@@ -7,10 +7,10 @@ app.use(bodyParser.urlencoded({extended:true}));
 app.use(express.static("public"));
 
 app.get("/",(req,res)=>{
-    res.render("page0.ejs.");
+    res.render("page0.ejs");
 })
 
 app.listen(port,()=>{
-    console.log(`erver running live on port ${3000}` );
+    console.log(`server running live on port ${3000}` );
 })
 
